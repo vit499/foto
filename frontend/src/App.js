14dd6ce03513df95256eaf8937fced8d0ecbbd41
vit-deb <d02@231.ru>
2020-11-19 17:42:49 -0500

    empty project react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import axios from "axios";
 
 function App() {
+  const getApiRequest = () => {
+    console.log("get api")
+    axios("/api/k").then(response => {
+      console.log('response', response);
+    });
+  }
+
   return (
     <div>
       <h3>react</h3>
+      <button onClick={getApiRequest}>get api</button>
     </div>
   );
 }
